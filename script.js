480945d4a6b3e4ac5ff5612a625331b0a052614c
Jimmy Colgan <jcolgan1@stevens.edu>
2024-12-22 00:38:03 -0500

    clearInp

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ document.getElementById('enterbutton').addEventListener('click', function () {
         document.getElementById('artist5').value
     ];
     console.log(inputs);
+    document.getElementById('artist1').value = '';
+    document.getElementById('artist2').value = '';
+    document.getElementById('artist3').value = '';
+    document.getElementById('artist4').value = '';
+    document.getElementById('artist5').value = '';
 });
