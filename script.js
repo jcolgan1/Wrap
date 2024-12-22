f7c6257abb5278010e8771c6de7bae622541eb5f
Jimmy Colgan <jimmycolgan@macbook-pro.myfiosgateway.com>
2024-12-22 00:22:40 -0500

    button

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('enterbutton').addEventListener('click', function () {
+    const inputs = [
+        document.getElementById('artist1').value,
+        document.getElementById('artist2').value,
+        document.getElementById('artist3').value,
+        document.getElementById('artist4').value,
+        document.getElementById('artist5').value
+    ];
+    console.log(inputs);
+});
